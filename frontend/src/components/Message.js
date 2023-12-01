329ca2936372c15e7213a86ac8f563cf2337d4f2
deepapaikar <deepapaikar19@gmail.com~>
2023-12-01 13:51:56 -0500

    base setup done

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,83 +1,105 @@
 import React from 'react'
 import axios from 'axios'
-class Message extends React.Component
-{
-    state ={
-        chat:[],
-        msg:''
+class Message extends React.Component {
+    state = {
+        chat: [],
+        msg: ''
     }
-    handleChange = (e)=>{
+    handleChange = (e) => {
         console.log(e.target.value);
-        this.setState({msg:e.target.value});
+        this.setState({ msg: e.target.value });
     }
-    handleSend = ()=>{
-        
-        
-        if(this.state.msg != '')
-        {
-            axios.post('http://127.0.0.1:5000/user',{'msg':this.state.msg})
-            .then(res=>{
-                let ch = this.state.chat;
-                ch.push({from:'our',msag:this.state.msg});
-                ch.push({from:'cb',msag:res.data});
-                this.setState({chat:ch,msg:''});
-                console.log(this.state);
-                
+    handleSend = () => {
 
-            })
-            .catch(err=>{
-                console.log(err);
-            });
-           //manual data feeding
-           let ch = this.state.chat;
-           ch.push({from:'our',msag:this.state.msg});
-           ch.push({from:'cb',msag:"Hi! I am good , how aboutyou ?"});
-           this.setState({chat:ch,msg:''});
-           console.log(this.state);
-           
-            
+
+        if (this.state.msg != '') {
+            axios.post('http://127.0.0.1:5000/user', { 'msg': this.state.msg })
+                .then(res => {
+                    let ch = this.state.chat;
+                    ch.push({ from: 'our', msag: this.state.msg });
+                    ch.push({ from: 'cb', msag: res.data });
+                    this.setState({ chat: ch, msg: '' });
+                    console.log(this.state);
+
+
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            //manual data feeding
+            let ch = this.state.chat;
+            ch.push({ from: 'our', msag: this.state.msg });
+            ch.push({ from: 'cb', msag: "Hi! I am good , how aboutyou ?" });
+            this.setState({ chat: ch, msg: '' });
+            console.log(this.state);
+
+
             this.forceUpdate();
         }
-        let interval = window.setInterval(function(){
+        let interval = window.setInterval(function () {
             var elem = document.getElementById('chatt');
             elem.scrollTop = elem.scrollHeight;
             window.clearInterval(interval);
-        },5000)
+        }, 5000)
     }
-    render()
-    {
-        return(
-            <div  >
-                <div id='chatt' style={{overflow:'scroll',overflowX:'hidden',height:'85vh'}}>
-                    {
-                        this.state.chat.map((msg)=>{
-                            if(msg.from == 'cb')
-                            {
-                                return <div style={{flexWrap:'wrap',fontSize:'25px',fontFamily:'cursive',
-                                marginBottom:'10px',borderRadius:'100px',marginRight:'500px',
-                                padding:'30px',paddingBottom:'20px',width:'30%',
-                                backgroundColor:'black',color:'white',float:'left',
-                                display:'block'}}>{msg.msag} </div>
-                            }
-                            else{
-                            return <div style={{flexWrap:'wrap',fontSize:'25px',fontFamily:'cursive',
-                            marginBottom:'10px',borderRadius:'100px',marginLeft:'500px',
-                            padding:'30px',paddingBottom:'20px',width:'30%',backgroundColor:'orange',
-                            float:'right',display:'block',color:'whitesmoke'}}>{msg.msag}</div>
-                            }
-                        })
-                    }
+    render() {
+        return (
+            <div id="fullscreen"  >
+                <div id="leftSideBar">
+                    <div class="information">
+                        <div class="agent-info">CodeGen</div>
+                        <div class="code-agent-automatically-review-llm-code">
+                            Code Agent, automatically review llm code
+                        </div>
+                    </div>
                 </div>
-                <div style={{height:'2vh'}}>
-                    <input type='text' name='msg' 
-                      onChange={(e)=>this.handleChange(e)} 
-                      class="form-control"
-                      
-                      style={{marginLeft:'150px',width:'80%',float:'left'}}
-                      value={this.state.msg} />
-                      <button onClick={()=>this.handleSend()} style={{paddingLeft:'25px',paddingRight:'25px'}} class="btn btn-primary">Send</button>
+                <div id="chatContainer">
+ 
+                    <div id='chatt' style={{ height: '75vh' }}>
+                        {
+                            this.state.chat.map((msg) => {
+                                if (msg.from == 'cb') {
+                                    return <div  id ="msgLeft" style={{
+                                        flexWrap: 'wrap', fontSize: '17px', fontFamily: 'cursive',
+                                        marginBottom: '10px', borderRadius: '5px', marginRight: '500px',
+                                        padding: '10px', paddingBottom: '10px', width: '70%',
+                                        backgroundColor: '#f9f9f9', color: 'black', float: 'left',
+                                        display: 'block'
+                                    }}>{msg.msag} </div>
+                                }
+                                else {
+                                    return <div style={{
+                                        flexWrap: 'wrap', fontSize: '17px', fontFamily: 'cursive', marginRight: '20px',
+                                        marginBottom: '10px', borderRadius: '5px', marginLeft: '500px',
+                                        padding: '10px', paddingBottom: '10px', width: '70%', backgroundColor: 'whitesmoke',
+                                        float: 'right', display: 'block', color: 'black'
+                                    }}>{msg.msag}</div>
+                                }
+                            })
+                        }
+                    </div>
+                    <div style={{ height: '2vh', }}>
+                        <input type='text' name='msg'
+                            onChange={(e) => this.handleChange(e)}
+                            class="form-control"
+
+                            style={{
+                                marginLeft: '250px', width: '70%', float: 'left', flex: 1,
+                                padding: '10px',
+                                border: '1px solid #ddd',
+                                borderRadius: '3px',
+                                marginRight: '10px'
+                            }}
+                            value={this.state.msg} />
+                        <button onClick={() => this.handleSend()} style={{
+                            paddingLeft: '25px', paddingRight: '25px', padding: '10px',
+                            border: '1px solid #ddd',
+                            borderRadius: '3px',
+                            marginRight: '10px', backgroundColor: '#5b9bd5', color: 'white', border: 'none'
+                        }} class="btn btn-primary">Send</button>
+                    </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -4,8 +4,8 @@ class Header extends React.Component
     render()
     {
         return(
-            <div style={{backgroundColor:'skyblue',width:'100%',height:'10%'}}>
-                <h1 style={{padding:'7px',margin:'0px',fontFamily:'cursive'}}>MY CODE GEN CHATBOT</h1>
+            <div style={{backgroundColor:'black',width:'100%',height:'10%'}}>
+                <h1 style={{padding:'7px',margin:'0px',fontFamily:'cursive', color:'white'}}>Welcome to Codegen</h1>
             </div>
         );
     }
