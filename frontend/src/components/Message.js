01ba6c84d17f10e5814a406a25a09dc7913eeaf4
deepapaikar <deepapaikar19@gmail.com~>
2023-12-03 14:28:35 -0500

    css changes

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import axios from 'axios'
+import { io } from 'socket.io-client';
+
+
+
 class Message extends React.Component {
     state = {
         chat: [],
@@ -10,10 +14,15 @@ class Message extends React.Component {
         this.setState({ msg: e.target.value });
     }
     handleSend = () => {
-
+        //socket connection
+        const socket = io(' http://127.0.0.1:5002');
+        socket.on('connect', () => {
+            console.log('Connected to the server');
+        });
 
         if (this.state.msg != '') {
-            axios.post('http://127.0.0.1:5000/user', { 'msg': this.state.msg })
+            socket.emit('message', { content: this.state.msg });
+            /*axios.post('http://127.0.0.1:5000/user', { 'msg': this.state.msg })
                 .then(res => {
                     let ch = this.state.chat;
                     ch.push({ from: 'our', msag: this.state.msg });
@@ -32,8 +41,15 @@ class Message extends React.Component {
             ch.push({ from: 'cb', msag: "Hi! I am good , how aboutyou ?" });
             this.setState({ chat: ch, msg: '' });
             console.log(this.state);
-
-
+            */
+            socket.on('message', data => {
+                console.log('Received message:', data);
+            });
+            let ch = this.state.chat;
+            ch.push({ from: 'our', msag: this.state.msg });
+            ch.push({ from: 'cb', msag: "Hi! I am good , how aboutyou ?" });
+            this.setState({ chat: ch, msg: '' });
+            console.log(this.state);
             this.forceUpdate();
         }
         let interval = window.setInterval(function () {
@@ -55,48 +71,26 @@ class Message extends React.Component {
                 </div>
                 <div id="chatContainer">
 
-                    <div id='chatt' style={{ height: '75vh', marginLeft: '20px', marginRight: '20px' }}>
+                    <div id='chatt' >
                         {
                             this.state.chat.map((msg) => {
                                 if (msg.from == 'cb') {
-                                    return <div id="msgLeft" style={{
-                                        flexWrap: 'wrap', fontSize: '17px', fontFamily: 'cursive',
-                                        marginBottom: '10px', borderRadius: '5px', marginRight: '500px',
-                                        padding: '10px', paddingBottom: '10px',
-                                        backgroundColor: '#f9f9f9', color: 'black', float: 'left',
-                                        display: 'block'
-                                    }}>{msg.msag} </div>
+                                    return <div id="botWindow" >{msg.msag} </div>
                                 }
                                 else {
-                                    return <div style={{
-                                        flexWrap: 'wrap', fontSize: '17px', fontFamily: 'cursive', marginRight: '20px',
-                                        marginBottom: '10px', borderRadius: '5px', marginLeft: '500px',
-                                        padding: '10px', paddingBottom: '10px', backgroundColor: 'whitesmoke',
-                                        float: 'right', display: 'block', color: 'black'
-                                    }}>{msg.msag}</div>
+                                    return <div id="userWindow" >{msg.msag}</div>
                                 }
                             })
                         }
                     </div>
-                    <div style={{ height: '2vh', }}>
-                        <input type='text' name='msg'
+                    <div id="chatCont" >
+                        <input type='text' name='msg' id="msgText"
                             onChange={(e) => this.handleChange(e)}
                             class="form-control"
-
-                            style={{
-                                marginLeft: '20px', width: '70%', float: 'left', flex: 1,
-                                padding: '10px',
-                                border: '1px solid #ddd',
-                                borderRadius: '3px',
-                                marginRight: '10px'
-                            }}
                             value={this.state.msg} />
-                        <button onClick={() => this.handleSend()} style={{
-                            paddingLeft: '25px', paddingRight: '25px', padding: '10px',
-                            border: '1px solid #ddd',
-                            borderRadius: '3px',
-                            marginRight: '10px', backgroundColor: '#5b9bd5', color: 'white', border: 'none', width: '100px'
-                        }} class="btn btn-primary">Send</button>
+                        <button id="submitBtn" onClick={() => this.handleSend()}
+                            type="submit"
+                            class="btn btn-primary">Send</button>
                     </div>
                 </div>
             </div >
